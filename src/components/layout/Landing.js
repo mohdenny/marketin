409a415ca2dcb9ef25f1dc5a6cdfc7f13605edb7
Mohammad Denny <mohammaddenny94@gmail.com>
2021-12-05 18:54:29 +0700

    font footer set light

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -167,7 +167,7 @@ const Landing = () => {
 
             <footer className="bg-purple-600 h-auto w-full">
                 <div className="container mx-auto px-4 py-2 h-full w-full text-white text-center space-y-3">
-                    <p className="text-4xl font-thin">Marketing First</p>
+                    <p className="text-4xl font-light">Marketing First</p>
                     <p className="text-sm">© Copyright 2021. Marketin, Inc. All Rights Reserved. Legal Privacy</p>
                 </div>
             </footer>
